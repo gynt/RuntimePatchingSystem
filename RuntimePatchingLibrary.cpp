6b659a99ad58c776765092b307554b1c804ca9cc
gynt <gynt@users.noreply.github.com>
2021-04-18 22:55:28 +0200

    removed commetns

--- RuntimePatchingLibrary.cpp
+++ RuntimePatchingLibrary.cpp
@@ -1,24 +1,5 @@
-// RuntimePatchingLibrary.cpp : Defines the exported functions for the DLL.
-//
-
 #include "framework.h"
 #include "RuntimePatchingLibrary.h"
-
-
-//// This is an example of an exported variable
-//RUNTIMEPATCHINGLIBRARY_API int nRuntimePatchingLibrary=0;
-//
-//// This is an example of an exported function.
-//RUNTIMEPATCHINGLIBRARY_API int fnRuntimePatchingLibrary(void)
-//{
-//    return 0;
-//}
-//
-//// This is the constructor of a class that has been exported.
-//CRuntimePatchingLibrary::CRuntimePatchingLibrary()
-//{
-//    return;
-//}
 
 RUNTIMEPATCHINGLIBRARY_API void RPL_initialize(std::string bootstrapFilePath, std::string packagePath) {
 	return LuaAPI::initialize(bootstrapFilePath, packagePath);
--- RuntimePatchingLibrary.h
+++ RuntimePatchingLibrary.h
@@ -1,9 +1,3 @@
-// The following ifdef block is the standard way of creating macros which make exporting
-// from a DLL simpler. All files within this DLL are compiled with the RUNTIMEPATCHINGLIBRARY_EXPORTS
-// symbol defined on the command line. This symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see
-// RUNTIMEPATCHINGLIBRARY_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
 #ifdef RUNTIMEPATCHINGLIBRARY_EXPORTS
 #define RUNTIMEPATCHINGLIBRARY_API __declspec(dllexport)
 #else
@@ -12,17 +6,6 @@
 
 #include <string>
 #include "LuaAPI.h"
-
-//// This class is exported from the dll
-//class RUNTIMEPATCHINGLIBRARY_API CRuntimePatchingLibrary {
-//public:
-//	CRuntimePatchingLibrary(void);
-//	// TODO: add your methods here.
-//};
-//
-//extern RUNTIMEPATCHINGLIBRARY_API int nRuntimePatchingLibrary;
-//
-//RUNTIMEPATCHINGLIBRARY_API int fnRuntimePatchingLibrary(void);
 
 RUNTIMEPATCHINGLIBRARY_API void RPL_initialize(std::string bootstrapFilePath, std::string packagePath);
 
