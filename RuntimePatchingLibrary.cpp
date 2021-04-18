35f767f2968732b316c72b9757eccccf685f3d2c
gynt <gynt@users.noreply.github.com>
2021-04-19 00:11:50 +0200

    fixed typos

--- RuntimePatchingLibrary.cpp
+++ RuntimePatchingLibrary.cpp
@@ -1,5 +1,6 @@
 #include "framework.h"
 #include "RuntimePatchingLibrary.h"
+#include "LuaAPI.h"
 
 RUNTIMEPATCHINGLIBRARY_API void RPL_initialize(std::string bootstrapFilePath, std::string packagePath) {
 	return LuaAPI::initialize(bootstrapFilePath, packagePath);
@@ -25,14 +26,14 @@ RUNTIMEPATCHINGLIBRARY_API void RPL_initializeLuaAPI(lua_State* L, bool includeP
 	return LuaAPI::initializeLuaAPI(L, includePrintRedirect);
 }
 
-RUNTIMEPATCHINGLIBRARY_API void setupPackagePath(lua_State* L, std::string packagePath) {
+RUNTIMEPATCHINGLIBRARY_API void RPL_setupPackagePath(lua_State* L, std::string packagePath) {
 	return LuaAPI::setupPackagePath(L, packagePath);
 }
 
-RUNTIMEPATCHINGLIBRARY_API void runBootstrapFile(lua_State* L, std::string bootstrapFilePath) {
+RUNTIMEPATCHINGLIBRARY_API void RPL_runBootstrapFile(lua_State* L, std::string bootstrapFilePath) {
 	return LuaAPI::runBootstrapFile(L, bootstrapFilePath);
 }
 
-RUNTIMEPATCHINGLIBRARY_API void initializeLua() {
+RUNTIMEPATCHINGLIBRARY_API void RPL_initializeLua() {
 	return LuaAPI::initializeLua();
 }
