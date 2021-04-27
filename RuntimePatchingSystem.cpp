05210fa1eefd7efcf6d1b32ed1f8bb14ac718a43
gynt <gynt@users.noreply.github.com>
2021-04-27 15:46:39 +0200

    completed renaming RPL to RPS

--- RuntimePatchingSystem.cpp
+++ RuntimePatchingSystem.cpp
@@ -2,38 +2,38 @@
 #include "RuntimePatchingSystem.h"
 #include "LuaAPI.h"
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_initialize(std::string bootstrapFilePath, std::string packagePath) {
+RUNTIMEPATCHINGSYSTEM_API void RPS_initialize(std::string bootstrapFilePath, std::string packagePath) {
 	return LuaAPI::initialize(bootstrapFilePath, packagePath);
 }
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_deinitialize() {
+RUNTIMEPATCHINGSYSTEM_API void RPS_deinitialize() {
 	return LuaAPI::deinitialize();
 }
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_executeSnippet(std::string code) {
+RUNTIMEPATCHINGSYSTEM_API void RPS_executeSnippet(std::string code) {
 	return LuaAPI::executeSnippet(code);
 }
 
-RUNTIMEPATCHINGSYSTEM_API int RPL_getCurrentStackSize() {
+RUNTIMEPATCHINGSYSTEM_API int RPS_getCurrentStackSize() {
 	return LuaAPI::getCurrentStackSize();
 }
 
-RUNTIMEPATCHINGSYSTEM_API lua_State* RPL_getLuaState() {
+RUNTIMEPATCHINGSYSTEM_API lua_State* RPS_getLuaState() {
 	return LuaAPI::getLuaState();
 }
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_initializeLuaAPI(lua_State* L, bool includePrintRedirect) {
+RUNTIMEPATCHINGSYSTEM_API void RPS_initializeLuaAPI(lua_State* L, bool includePrintRedirect) {
 	return LuaAPI::initializeLuaAPI(L, includePrintRedirect);
 }
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_setupPackagePath(lua_State* L, std::string packagePath) {
+RUNTIMEPATCHINGSYSTEM_API void RPS_setupPackagePath(lua_State* L, std::string packagePath) {
 	return LuaAPI::setupPackagePath(L, packagePath);
 }
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_runBootstrapFile(lua_State* L, std::string bootstrapFilePath) {
+RUNTIMEPATCHINGSYSTEM_API void RPS_runBootstrapFile(lua_State* L, std::string bootstrapFilePath) {
 	return LuaAPI::runBootstrapFile(L, bootstrapFilePath);
 }
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_initializeLua() {
+RUNTIMEPATCHINGSYSTEM_API void RPS_initializeLua() {
 	return LuaAPI::initializeLua();
 }
--- RuntimePatchingSystem.h
+++ RuntimePatchingSystem.h
@@ -7,23 +7,23 @@
 #include <string>
 #include "lua.hpp"
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_initialize(std::string bootstrapFilePath, std::string packagePath);
+RUNTIMEPATCHINGSYSTEM_API void RPS_initialize(std::string bootstrapFilePath, std::string packagePath);
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_deinitialize();
+RUNTIMEPATCHINGSYSTEM_API void RPS_deinitialize();
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_executeSnippet(std::string code);
+RUNTIMEPATCHINGSYSTEM_API void RPS_executeSnippet(std::string code);
 
-RUNTIMEPATCHINGSYSTEM_API int RPL_getCurrentStackSize();
+RUNTIMEPATCHINGSYSTEM_API int RPS_getCurrentStackSize();
 
-RUNTIMEPATCHINGSYSTEM_API lua_State* RPL_getLuaState();
+RUNTIMEPATCHINGSYSTEM_API lua_State* RPS_getLuaState();
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_initializeLuaAPI(lua_State* L, bool includePrintRedirect);
+RUNTIMEPATCHINGSYSTEM_API void RPS_initializeLuaAPI(lua_State* L, bool includePrintRedirect);
 
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_initializeLuaAPI(lua_State* L, bool includePrintRedirect);
+RUNTIMEPATCHINGSYSTEM_API void RPS_initializeLuaAPI(lua_State* L, bool includePrintRedirect);
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_setupPackagePath(lua_State* L, std::string packagePath);
+RUNTIMEPATCHINGSYSTEM_API void RPS_setupPackagePath(lua_State* L, std::string packagePath);
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_runBootstrapFile(lua_State* L, std::string bootstrapFilePath);
+RUNTIMEPATCHINGSYSTEM_API void RPS_runBootstrapFile(lua_State* L, std::string bootstrapFilePath);
 
-RUNTIMEPATCHINGSYSTEM_API void RPL_initializeLua();
+RUNTIMEPATCHINGSYSTEM_API void RPS_initializeLua();
